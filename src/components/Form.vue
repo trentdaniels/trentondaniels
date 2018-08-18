16653f71615c948614e658082fe74bc080dba46b
Trenton Daniels <trentondaniels95@gmail.com>
2018-08-18 18:04:29 -0500

    added animations to home screen slider

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -2,16 +2,28 @@
     <div class="form">
         <h1>Let's link.</h1>
         <form id="form">
-            <label>What's your name?</label>
-            <input type="text" id="name" v-model="name" placeholder="ex: John Doe"/>
-            <p>{{ name }}</p>
-            <label>Email?</label>
-            <input type="email" id="email" v-model="email" placeholder="email@email.com"/>
-            <p>{{ email }}</p>
-            <label>What can I do for you?</label>
-            <textarea v-model="description" placeholder="I want to work together.."></textarea><br>
-            <button type="submit">Link Up</button>
-            
+            <div>
+                <label for="firstName">First Name: </label>
+                <input type="text" id="firstName" v-model="userData.firstName" placeholder="ex: John Doe"/>
+                <p>{{ userData.firstName }}</p>
+            </div>
+            <div>
+                <label for="lastName">Last Name: </label>
+                <input type="text" id="lastName" v-model="userData.lastName" placeholder="ex: John Doe"/>
+                <p>{{ userData.lastName }}</p>
+            </div>
+            <div>
+                <label for="email">Email: </label>
+                <input type="email" id="email" v-model="userData.email" placeholder="email@email.com"/>
+                <p>{{ userData.email }}</p>
+            </div>
+            <div>
+                <label for="description">Message: </label>
+                <textarea id="description" v-model="userData.description" placeholder="I want to work together.."></textarea>
+            </div>
+            <div>
+                <button type="submit">Link Up</button>
+            </div>
         </form>
     </div>
 </template>
@@ -21,11 +33,22 @@ export default {
     name: 'Form',
     data: function () {
         return {
-            name: '',
-            email: '',
-            description: ''
+            
+            userData: {
+                firstName: '',
+                lastName: '',
+                email: '',
+                description: ''
+            }
         }
-    }
+    },
+    
 }
 </script>
+
+<style lang="scss" scoped>
+textarea {
+    white-space: pre;
+}
+</style>
 
--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="project-list">
     <h1>{{ msg }}</h1>
-    <div class="project" v-for="project in projects" :key="project.id" v-if="project.id === projectNumber">
-      <h2>{{ project.name }}</h2>
-      <p>{{ project.description }}</p>
-    </div>
+    <transition name="fade-up" mode="out-in">
+      <div class="project" v-for="project in projects" :key="project.id" v-if="project.id === projectNumber">
+        <h2>{{ project.name }}</h2>
+        <p>{{ project.description }}</p>
+      </div>
+    </transition>
     <button @click="nextProject">Next</button>
     <button @click="prevProject">Previous</button>
   </div>
@@ -31,16 +33,40 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h2 {
+
+.project {
+  text-align: center;
+  h2 {
   text-align: center;
   margin: 40px 0 auto;
-}
-p {
+  }
+  p {
   text-align: center;
   width: 50%;
   margin: auto;
+  }
 }
-.project {
-  text-align: center;
+.fade-up-enter {
+  opacity: 0;
 }
+
+.fade-up-enter-active {
+  transition: opacity .5s;
+}
+
+.fade-up-leave {
+  opacity: 1;
+}
+
+.fade-up-leave-active {
+  transition: opacity .55s;
+  opacity: 0;
+}
+
+@keyframes fade-up {
+  0% {transfrom: translateY(0px); opacity: 1}
+  100% {transform: translateY(10px); opacity: 0}
+}
+
+
 </style>
